ea9dd87f3a3d71e3e88a8bafc76c1bf611cb56ef
Guillaume Gay <guillaume@damcb.com>
2019-01-12 12:32:49 +0100

    build files to configure cgal

---
+++ recipe/bld.bat
@@ -0,0 +1,7 @@
+
+cd CGAL
+cmake -DCGAL_HEADER_ONLY=ON ^
+      -DWITH_CGAL_ImageIO=OFF ^
+      -DWITH_CGAL_Qt5=OFF .
+cd ..
+python setup.py install --single-version-externally-managed --record record.txt
